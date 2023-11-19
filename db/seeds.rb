bd47b66a3a5c54ed3cefb398cf71472828773b43
thomas laizé <thomas33130@hotmail.fr>
2023-11-19 11:09:42 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "chinese"}
+a = {name: "a", address: "a", category: "chinese"}
+b = {name: "b", address: "b", category: "chinese"}
+c = {name: "c", address: "c", category: "chinese"}
+
+[dishoom, pizza_east, a, b, c].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
